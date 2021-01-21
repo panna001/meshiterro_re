9890a5633aef6da38140217b0d4c33646dcbf38c
EC2 Default User <ec2-user@ip-172-31-92-133.ec2.internal>
2021-01-21 04:20:52 +0000

    [Finish]5-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :confiqure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resourse)
+    post_images_path
+  end
+  
   
   protected
-  
+
   def confiqure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a748bb8fc1d1b998e878794c0d7ba3446e9bd5dd7df60e99f0ce8952fbcdfd10111581f16e27b6b182ff6977933d851b98d1a5341869dcf1f12163c259d84f91'
