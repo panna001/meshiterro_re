3ad89543baa4f9296f47f86eb1e54f26a19dc4ae
EC2 Default User <ec2-user@ip-172-31-92-133.ec2.internal>
2021-01-21 13:25:00 +0000

    [Start]5-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :confiqure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top]
   def after_sign_in_path_for(resourse)
     post_images_path
   end
-  
-  
+
+
   protected
 
   def confiqure_permitted_parameters
